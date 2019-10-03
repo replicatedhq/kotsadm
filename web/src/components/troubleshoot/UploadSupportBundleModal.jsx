41d12b6b0a3d579b6a43f10e149cd091d2cdd08a
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2019-10-03 17:11:02 +0000

    updates

--- web/src/components/troubleshoot/UploadSupportBundleModal.jsx
+++ web/src/components/troubleshoot/UploadSupportBundleModal.jsx
@@ -11,9 +11,7 @@ class UploadSupportBundleModal extends React.Component {
   constructor() {
     super();
     this.state = {
-      successState: false,
       fileUploading: false,
-      newBundleId: "",
       supportBundle: {},
     };
   }
@@ -21,11 +19,10 @@ class UploadSupportBundleModal extends React.Component {
   uploadAndAnalyze = async () => {
     try {
       const { watch } = this.props;
-      const { newBundleId } = this.state;
-      const bundleId = newBundleId !== "" ? newBundleId : randomstring.generate({ capitalization: "lowercase" });
+      const bundleId = randomstring.generate({ capitalization: "lowercase" });
       const uploadBundleUrl = `${window.env.TROUBLESHOOT_ENDPOINT}/${watch.id}/${bundleId}`;
 
-      this.setState({ fileUploading: true, newBundleId: bundleId });
+      this.setState({ fileUploading: true });
 
       const response = await fetch(uploadBundleUrl, {
         method: "PUT",
@@ -37,8 +34,8 @@ class UploadSupportBundleModal extends React.Component {
       const analyzedBundle = await response.json();
 
       this.setState({ fileUploading: false });
-      if (this.props.submitCallback && typeof this.props.submitCallback === "function") {
-        this.props.submitCallback(analyzedBundle.id);
+      if (this.props.onBundleUploaded) {
+        this.props.onBundleUploaded(analyzedBundle.id);
       }
     } catch (err) {
       this.setState({ fileUploading: false });
@@ -85,71 +82,56 @@ class UploadSupportBundleModal extends React.Component {
   }
 
   render() {
-    const { successState, newBundleId, supportBundle, fileUploading } = this.state;
+    const { supportBundle, fileUploading } = this.state;
     const hasFile = supportBundle && !isEmpty(supportBundle);
 
     return (
       <div className="console">
         <div id="UploadSupportBundleModal">
-          {successState ?
-            <div className="UploadSuccess-wrapper u-textAlign--center">
-              <div className="analysis-illustration-wrapper u-marginBottom--20">
-                <div className="icon u-analyzingBundleIcon u-position--relative">
-                  <div className="icon u-analyzingBundleMagnifyingGlassIcon magifying-glass-animate"></div>
-                </div>
-              </div>
-              <p className="u-fontSize--largest u-fontWeight--bold u-lineHeight--default u-color--tuna u-marginBottom--normal">Your bundle has been uploaded!</p>
-              <p className="u-fontSize--normal u-fontWeight--normal u-lineHeight--normal u-color--dustyGray">We've begun and analysis of your support bundle. Check out the analysis page for a breakdown of this support bundle</p>
-              <div className="button-wrapper">
-                <Link to={`/troubleshoot/analyze/${newBundleId}`} className="btn primary">View bundle analysis</Link>
-              </div>
-            </div>
-            :
-            <div>
-              <p className="u-fontSize--largest u-fontWeight--bold u-lineHeight--default u-color--tuna u-marginBottom--small">
-                Upload a support bundle
+          <div>
+            <p className="u-fontSize--largest u-fontWeight--bold u-lineHeight--default u-color--tuna u-marginBottom--small">
+              Upload a support bundle
               </p>
-              <p className="u-fontSize--normal u-fontWeight--medium u-lineHeight--normal u-color--dustyGray">
-                Upload a support bundle from your environment to visually analyze the server and receive insights about the server, the network and your application.
+            <p className="u-fontSize--normal u-fontWeight--medium u-lineHeight--normal u-color--dustyGray">
+              Upload a support bundle from your environment to visually analyze the server and receive insights about the server, the network and your application.
               </p>
-              <div className="u-marginTop--20">
-                <div>
-                  <div className={`FileUpload-wrapper ${hasFile ? "has-file" : ""}`}>
-                    <Dropzone
-                      className="Dropzone-wrapper"
-                      accept="application/gzip, .gz"
-                      onDropAccepted={this.onDrop}
-                      multiple={false}
-                    >
-                      {hasFile ?
-                        <p className="u-fontSize--normal u-fontWeight--medium">{supportBundle.name}</p>
-                        :
-                        <div className="u-textAlign--center">
-                          <span className="icon u-TarFileIcon u-marginBottom--20"></span>
-                          <p className="u-fontSize--normal u-color--dustyGray u-fontWeight--medium u-lineHeight--normal">Drag your bundle here or <span className="u-color--astral u-fontWeight--medium u-textDecoration--underlineOnHover">choose a file to upload</span></p>
-                          <p className="u-fontSize--normal u-color--dustyGray u-fontWeight--normal u-lineHeight--normal u-marginTop--small">This file can be any
+            <div className="u-marginTop--20">
+              <div>
+                <div className={`FileUpload-wrapper ${hasFile ? "has-file" : ""}`}>
+                  <Dropzone
+                    className="Dropzone-wrapper"
+                    accept="application/gzip, .gz"
+                    onDropAccepted={this.onDrop}
+                    multiple={false}
+                  >
+                    {hasFile ?
+                      <p className="u-fontSize--normal u-fontWeight--medium">{supportBundle.name}</p>
+                      :
+                      <div className="u-textAlign--center">
+                        <span className="icon u-TarFileIcon u-marginBottom--20"></span>
+                        <p className="u-fontSize--normal u-color--dustyGray u-fontWeight--medium u-lineHeight--normal">Drag your bundle here or <span className="u-color--astral u-fontWeight--medium u-textDecoration--underlineOnHover">choose a file to upload</span></p>
+                        <p className="u-fontSize--normal u-color--dustyGray u-fontWeight--normal u-lineHeight--normal u-marginTop--small">This file can be any
                               <span className="u-fontWeight--medium u-color--astral u-textDecoration--underlineOnHover" onClick={this.openReplicatedSupportBundleLink}> Replicated Support Bundle </span>
-                          </p>
-                        </div>
-                      }
-                    </Dropzone>
-                  </div>
-                  <div className="u-marginTop--normal">
-                    <div className="FormButton-wrapper flex justifyContent--center">
-                      <button
-                        type="button"
-                        className="btn secondary flex-auto"
-                        onClick={this.uploadAndAnalyze}
-                        disabled={fileUploading || !hasFile}
-                      >
-                        {fileUploading ? "Uploading" : "Upload support bundle"}
-                      </button>
-                    </div>
+                        </p>
+                      </div>
+                    }
+                  </Dropzone>
+                </div>
+                <div className="u-marginTop--normal">
+                  <div className="FormButton-wrapper flex justifyContent--center">
+                    <button
+                      type="button"
+                      className="btn secondary flex-auto"
+                      onClick={this.uploadAndAnalyze}
+                      disabled={fileUploading || !hasFile}
+                    >
+                      {fileUploading ? "Uploading" : "Upload support bundle"}
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
-          }
+          </div>
         </div>
       </div>
     );
