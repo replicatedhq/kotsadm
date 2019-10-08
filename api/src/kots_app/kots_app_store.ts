11dd622fa27eaba1f63186737138e93b57d8214a
Ben Junya <ben@replicated.com>
2019-10-08 17:48:48 +0000

    Move getReleaseNotes to its own function

--- api/src/kots_app/kots_app_store.ts
+++ api/src/kots_app/kots_app_store.ts
@@ -160,25 +160,21 @@ export class KotsAppStore {
 
     q =
       `SELECT
-         app_downstream_version.created_at,
-         app_downstream_version.version_label,
-         app_downstream_version.status,
-         app_downstream_version.sequence,
-         app_downstream_version.applied_at,
-         app_downstream_version.preflight_result,
-         app_downstream_version.preflight_result_created_at,
-         app_version.release_notes AS release_notes
+         created_at,
+         version_label,
+         status,
+         sequence,
+         applied_at,
+         preflight_result,
+         preflight_result_created_at
         FROM
           app_downstream_version
-          LEFT JOIN app_version ON
-            app_downstream_version.sequence = app_version.sequence AND
-            app_downstream_version.app_id = app_version.app_id
         WHERE
-          app_downstream_version.app_id = $1 AND
-          app_downstream_version.cluster_id = $3 AND
-          app_downstream_version.sequence < $2
+          app_id = $1 AND
+          cluster_id = $3 AND
+          sequence < $2
         ORDER BY
-          app_downstream_version.sequence DESC`;
+          sequence DESC`;
     v = [
       appId,
       sequence,
@@ -189,7 +185,19 @@ export class KotsAppStore {
     const versionItems: KotsVersion[] = [];
 
     for (const row of result.rows) {
-      versionItems.push(this.mapKotsAppVersion(row));
+      const releaseNotes = await this.getReleaseNotes(appId, row.sequence);
+
+      let versionItem: KotsVersion = {
+        title: row.version_label,
+        status: row.status,
+        createdOn: row.created_at,
+        sequence: row.sequence,
+        deployedAt: row.applied_at,
+        releaseNotes: releaseNotes || "",
+        preflightResult: row.preflight_result,
+        preflightResultCreatedAt: row.preflight_result_created_at
+      };
+      versionItems.push(versionItem);
     }
 
     return versionItems;
@@ -215,25 +223,21 @@ export class KotsAppStore {
 
     q =
       `SELECT
-         app_downstream_version.created_at,
-         app_downstream_version.version_label,
-         app_downstream_version.status,
-         app_downstream_version.sequence,
-         app_downstream_version.applied_at,
-         app_downstream_version.preflight_result,
-         app_downstream_version.preflight_result_created_at,
-         app_version.release_notes AS release_notes
+         created_at,
+         version_label,
+         status,
+         sequence,
+         applied_at,
+         preflight_result,
+         preflight_result_created_at
         FROM
           app_downstream_version
-          LEFT JOIN app_version ON
-            app_downstream_version.sequence = app_version.sequence AND
-            app_downstream_version.app_id = app_version.app_id
         WHERE
-          app_downstream_version.app_id = $1 AND
-          app_downstream_version.cluster_id = $3 AND
-          app_downstream_version.sequence > $2
+          app_id = $1 AND
+          cluster_id = $3 AND
+          sequence > $2
         ORDER BY
-          app_downstream_version.sequence DESC`;
+          sequence DESC`;
 
     v = [
       appId,
@@ -245,7 +249,19 @@ export class KotsAppStore {
     const versionItems: KotsVersion[] = [];
 
     for (const row of result.rows) {
-      versionItems.push(this.mapKotsAppVersion(row));
+      const releaseNotes = await this.getReleaseNotes(appId, row.sequence);
+
+      let versionItem: KotsVersion = {
+        title: row.version_label,
+        status: row.status,
+        createdOn: row.created_at,
+        sequence: row.sequence,
+        deployedAt: row.applied_at,
+        releaseNotes: releaseNotes || "",
+        preflightResult: row.preflight_result,
+        preflightResultCreatedAt: row.preflight_result_created_at
+      };
+      versionItems.push(versionItem);
     }
 
     return versionItems;
@@ -269,25 +285,21 @@ export class KotsAppStore {
 
     q =
       `SELECT
-         app_downstream_version.created_at,
-         app_downstream_version.version_label,
-         app_downstream_version.status,
-         app_downstream_version.sequence,
-         app_downstream_version.applied_at,
-         app_downstream_version.preflight_result,
-         app_downstream_version.preflight_result_created_at,
-         app_version.release_notes AS release_notes
+         created_at,
+         version_label,
+         status,
+         sequence,
+         applied_at,
+         preflight_result,
+         preflight_result_created_at
         FROM
           app_downstream_version
-          LEFT JOIN app_version ON
-            app_downstream_version.sequence = app_version.sequence AND
-            app_downstream_version.app_id = app_version.app_id
         WHERE
-          app_downstream_version.app_id = $1 AND
-          app_downstream_version.cluster_id = $3 AND
-          app_downstream_version.sequence = $2
+          app_id = $1 AND
+          cluster_id = $3 AND
+          sequence = $2
         ORDER BY
-          app_downstream_version.sequence DESC`;
+          sequence DESC`;
     v = [
       appId,
       sequence,
@@ -295,8 +307,24 @@ export class KotsAppStore {
     ];
 
     result = await this.pool.query(q, v);
+    const row = result.rows[0];
 
-    const versionItem = this.mapKotsAppVersion(result.rows[0]);
+    if (!row) {
+      throw new ReplicatedError(`App Version for clusterId ${clusterId} not found. appId: ${appId}, sequence ${sequence}`);
+    }
+
+    const releaseNotes = await this.getReleaseNotes(appId, sequence);
+
+    let versionItem: KotsVersion = {
+      title: row.version_label,
+      status: row.status,
+      createdOn: row.created_at,
+      sequence: row.sequence,
+      deployedAt: row.applied_at,
+      releaseNotes: releaseNotes || "",
+      preflightResult: row.preflight_result,
+      preflightResultCreatedAt: row.preflight_result_created_at
+    };
 
     return versionItem;
   }
@@ -362,6 +390,17 @@ export class KotsAppStore {
     return result.rows[0].update_cursor;
   }
 
+  async getReleaseNotes(appId: string, sequence: number): Promise<string | undefined> {
+    const q = `SELECT release_notes FROM app_version WHERE app_id = $1 AND sequence = $2`;
+    const v = [
+      appId,
+      sequence
+    ];
+    const result = await this.pool.query(q,v);
+
+    return result.rows[0].release_notes;
+  }
+
   async getCurrentVersion(appId: string, clusterId: string): Promise<KotsVersion | undefined> {
     let q = `select current_sequence from app_downstream where app_id = $1 and cluster_id = $2`;
     let v = [
@@ -380,25 +419,22 @@ export class KotsAppStore {
 
     q =
       `SELECT
-         app_downstream_version.created_at,
-         app_downstream_version.version_label,
-         app_downstream_version.status,
-         app_downstream_version.sequence,
-         app_downstream_version.applied_at,
-         app_downstream_version.preflight_result,
-         app_downstream_version.preflight_result_created_at,
-         app_version.release_notes AS release_notes
+         created_at,
+         version_label,
+         status,
+         sequence,
+         applied_at,
+         preflight_result,
+         preflight_result_created_at
         FROM
           app_downstream_version
-          LEFT JOIN app_version ON
-            app_downstream_version.sequence = app_version.sequence AND
-            app_downstream_version.app_id = app_version.app_id
         WHERE
-          app_downstream_version.app_id = $1 AND
-          app_downstream_version.cluster_id = $3 AND
-          app_downstream_version.sequence = $2
+          app_id = $1 AND
+          cluster_id = $3 AND
+          sequence = $2
         ORDER BY
-          app_downstream_version.sequence DESC`;
+          sequence DESC`;
+
     v = [
       appId,
       sequence,
@@ -406,7 +442,24 @@ export class KotsAppStore {
     ];
 
     result = await this.pool.query(q, v);
-    const versionItem = this.mapKotsAppVersion(result.rows[0]);
+    const row = result.rows[0];
+
+    if (!row) {
+      throw new ReplicatedError(`App Version for clusterId ${clusterId} not found. appId: ${appId}, sequence ${sequence}`);
+    }
+
+    const releaseNotes = await this.getReleaseNotes(appId, sequence);
+
+    let versionItem: KotsVersion = {
+      title: row.version_label,
+      status: row.status,
+      createdOn: row.created_at,
+      sequence: row.sequence,
+      deployedAt: row.applied_at,
+      releaseNotes: releaseNotes || "",
+      preflightResult: row.preflight_result,
+      preflightResultCreatedAt: row.preflight_result_created_at
+    };
 
     return versionItem;
   }
