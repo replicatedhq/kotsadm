7854fbd99a6d7bbd2d51b47fc84cb6b876191d74
Andrew Reed <andrew@replicated.com>
2019-10-30 04:56:08 +0000

    purge master

--- api/src/kurl/resolvers/kurl_mutations.ts
+++ api/src/kurl/resolvers/kurl_mutations.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import * as _ from "lodash";
 import { Stores } from "../../schema/stores";
 import { Params } from "../../server/params";
@@ -5,11 +6,16 @@ import { ReplicatedError } from "../../server/errors";
 import { Context } from "../../context";
 import {
   KubeConfig,
+  AppsV1Api,
   CoreV1Api,
   V1beta1Eviction,
+  V1Node,
   V1OwnerReference,
   V1Pod } from "@kubernetes/client-node";
 import { logger } from "../../server/logger";
+import { IncomingMessage } from "http";
+import { Etcd3 } from "etcd3";
+import * as yaml from "js-yaml";
 
 export function KurlMutations(stores: Stores, params: Params) {
   return {
@@ -23,6 +29,8 @@ export function KurlMutations(stores: Stores, params: Params) {
 
     async deleteNode(root: any, { name }, context: Context) {
       context.requireSingleTenantSession();
+
+      await purge(name);
 
       return false;
     }
@@ -169,5 +177,129 @@ function isMirror(pod: V1Pod): boolean {
 function isDaemonSetPod(pod: V1Pod): boolean {
   return _.some(_.get(pod, "metadata.ownerReferences", []), (ownerRef: V1OwnerReference) => {
     return ownerRef.controller && ownerRef.kind === "DaemonSet";
+  });
+}
+
+async function purge(name: string) {
+  const kc = new KubeConfig();
+  kc.loadFromDefault();
+  const coreV1Client: CoreV1Api = kc.makeApiClient(CoreV1Api);
+  const appsV1Client: AppsV1Api = kc.makeApiClient(AppsV1Api);
+
+  let { response, body: node } = await coreV1Client.readNode(name);
+
+  if (response.statusCode !== 200) {
+    throw new ReplicatedError(`get node ${name} response: ${response.statusCode}`);
+  }
+
+  ({ response } = await coreV1Client.deleteNode(name));
+
+  if (response.statusCode !== 204) {
+    throw new Error(`Delete node returned status code ${response.statusCode}`);
+  }
+
+  await purgeOSD(coreV1Client, appsV1Client, name);
+
+  const isMaster = node.metadata!.labels && node.metadata!.labels!["node-role.kubernetes.io/master"];
+  if (isMaster) {
+    await purgeMaster(coreV1Client, node);
+  }
+}
+
+async function purgeMaster(coreV1Client: CoreV1Api, node: V1Node) {
+  const remainingMasterIPs: Array<string> = [];
+  let purgedMasterIP = "";
+
+  // 1. Remove the purged endpoint from kubeadm's list of API endpoints in the kubeadm-config 
+  // ConfigMap in the kube-system  namespace. Keep the list of all master IPs for step 2.
+  const configMapName = "kubeadm-config";
+  const configMapNS = "kube-system";
+  const configMapKey = "ClusterStatus";
+  const { response: cmResponse, body: kubeadmCM } = await coreV1Client.readNamespacedConfigMap(configMapName, configMapNS);
+  if (cmResponse.statusCode !== 200) {
+    throw new Error(`Get kubeadm-config map from kube-system namespace: ${cmResponse.statusCode}`);
+  }
+
+  const clusterStatus = yaml.safeLoad(kubeadmCM.data![configMapKey]);
+  _.each(clusterStatus.apiEndpoints, (obj, nodeName) => {
+    if (nodeName === node.metadata!.name) {
+      purgedMasterIP = obj.advertiseAddress;
+    } else {
+      remainingMasterIPs.push(obj.advertiseAddress);
+    }
+  });
+  delete(clusterStatus.apiEndpoints[node.metadata!.name!]);
+  kubeadmCM.data![configMapKey] = yaml.safeDump(clusterStatus);
+  await coreV1Client.replaceNamespacedConfigMap(configMapName, configMapNS, kubeadmCM);
+
+  // 2. Use the credentials from the mounted etcd client cert secret to connect to the remaining
+  // etcd members and tell them to forget the purged member.
+  const etcd = new Etcd3({
+    credentials: {
+      rootCertificate: fs.readFileSync("/etc/kubernetes/pki/etcd/tls.crt"),
+      privateKey: fs.readFileSync("/etc/kubernetes/pki/etcd/tls.key"),
+    },
+    hosts: _.map(remainingMasterIPs, (ip) => `https://${ip}:2379`),
+  });
+  const peerURL = `https://${purgedMasterIP}:2380`;
+}
+
+async function purgeOSD(coreV1Client: CoreV1Api, appsV1Client: AppsV1Api, nodeName: string) {
+  const namespace = "rook-ceph";
+  // 1. Find the Deployment for the OSD on the purged node and lookup its osd ID from labels
+  // before deleting it.
+  const osdLabelSelector = "app=rook-ceph-osd";
+  let { response, body: deployments } = await appsV1Client.listNamespacedDeployment(namespace, undefined, undefined, undefined, undefined, osdLabelSelector);
+
+  if (response.statusCode !== 200) {
+    throw new Error(`List osd deployments in rook-ceph namespace returned status code ${response.statusCode}`);
+  }
+
+  let osdID = "";
+
+  for (let i = 0; i < deployments.items.length; i++) {
+    const deploy = deployments.items[0];
+    const nodeSelectors = deploy.spec!.template!.spec!.nodeSelector;
+    const hostname = nodeSelectors ? nodeSelectors["kubernetes.io/hostname"] : "";
+
+    if (hostname === nodeName) {
+      osdID = deploy.metadata!.labels!["ceph-osd-id"];
+      ({ response } = await appsV1Client.deleteNamespacedDeployment(deploy.metadata!.name!, namespace, undefined, undefined, undefined, undefined, "Background"));
+      if (response.statusCode !== 200) {
+        throw new Error(`Got response status code ${response.statusCode}`);
+      }
+      break;
+    }
+  }
+
+  // 2. Using the osd ID discovered in step 1, exec into the Rook operator pod and run the ceph
+  // command to purge the OSD
+	const operatorLabelSelector = "app=rook-ceph-operator";
+  let { response: resp, body: pods } = await coreV1Client.listNamespacedPod(namespace, undefined, undefined, undefined, undefined, operatorLabelSelector);
+
+  if (resp.statusCode !== 200) {
+    throw new Error(`List operator pod in rook-ceph namespace returned status code ${resp.statusCode}`);
+  }
+
+  if (pods.items.length !== 1) {
+    logger.warn(`Found ${pods.items.length} rook operator pods: skipping osd purge command`);
+    return;
+  }
+  const pod = pods.items[0];
+  ({ response: resp } = await coreV1Client.connectPostNamespacedPodExec(pod.metadata!.name!, namespace, `ceph osd purge ${osdID}`));
+
+  if (resp.statusCode !== 200) {
+    const output = await readAll(resp);
+    throw new Error(`Failed to exec "ceph osd purge": ${output}`);
+  }
+}
+
+function readAll(stream: IncomingMessage): Promise<string> {
+  const chunks: Array<any> = [];
+
+  return new Promise((resolve, reject) => {
+    stream.on('data', chunk => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
   });
 }
