cc2519bcd19f0f06eddaf455cd904cbfcc5f943a
Andrew Reed <andrew@replicated.com>
2019-10-31 00:22:27 +0000

    revert query node

--- api/src/kurl/resolvers/kurl_queries.ts
+++ api/src/kurl/resolvers/kurl_queries.ts
@@ -41,9 +41,7 @@ export function KurlQueries(stores: Stores, params: Params) {
 
           return {
             name: item.metadata!.name,
-            // isConnected: true,
-            // TODO just want to trigger delete
-            isConnected: !(item.spec!.unschedulable),
+            isConnected: true,
             // TODO need to check for pods on the node
             canDelete: !!(item.spec!.unschedulable),
             kubeletVersion: item.status!.nodeInfo!.kubeletVersion,
