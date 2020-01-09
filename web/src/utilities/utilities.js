c5d0ab486ef717db33bc824fa4c41bfadcdbc937
Ethan Mosbaugh <ethan@replicated.com>
2020-01-09 18:48:20 +0000

    fe

--- web/src/utilities/utilities.js
+++ web/src/utilities/utilities.js
@@ -214,11 +214,13 @@ export function isAwaitingResults(versionHistory) {
 export function getPreflightResultState(preflightResults) {
   const results = preflightResults.results;
   let resultState = "pass";
-  for (const check of results) {
-    if (check.isWarn) {
-      resultState = "warn";
-    } else if (check.isFail) {
-      return "fail";
+  if (results) {
+    for (const check of results) {
+      if (check.isWarn) {
+        resultState = "warn";
+      } else if (check.isFail) {
+        return "fail";
+      }
     }
   }
   return resultState;
