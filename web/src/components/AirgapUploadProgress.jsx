6049890d5b1626f696aa0ad4a8ae14c24182ed43
divolgin <dmitriy@replicated.com>
2020-02-28 18:05:09 +0000

    Revert "clean console.logs"
    This reverts commit 3a44967fc2f4a8b9073e810e464d1720d6780925.

--- web/src/components/AirgapUploadProgress.jsx
+++ web/src/components/AirgapUploadProgress.jsx
@@ -71,6 +71,7 @@ function AirgapUploadProgress(props) {
   try {
     // Some of these messages will be JSON formatted progress reports.
     const jsonMessage = JSON.parse(statusMsg);
+    console.log(jsonMessage);
     const type = get(jsonMessage, "type");
     if (type === "progressReport") {
       statusMsg = jsonMessage.compatibilityMessage;
